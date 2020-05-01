83738e9074c88845dea3cc8fdb030820f6a74565
AkshayMohan <akshayceal@gmail.com>
2020-05-01 16:04:19 +0530

    created commands.js
    Script that contains client commands.

--- src/server/commands.js
+++ src/server/commands.js
@@ -1,1 +1,30 @@
-//Initial commit.
+const clientCommands = {
+
+	//command:				command function to be invoked.
+	'/join':				cmd_join
+}
+
+function cmd_join(client, socket, params) {
+
+	if(typeof client.clientName === 'string') {
+
+		socket.emit('server-message', 'ERROR : You are already joined with a nickname!');
+		return 1;
+	}
+	if(params === null) {
+
+		socket.emit('server-message', 'USAGE : /join [nickname]');
+		return 1;
+	} else {
+
+		client.clientName = params;
+		socket.join('chat-room');
+
+		socket.emit('server-message', 'Your nickname is now ' + params);
+
+		//Broadcast messages are sent to everyone except the client.
+		socket.broadcast.to('chat-room').emit('server-message', params + ' has joined the chat!');
+		return 0;
+	}
+}
+module.exports = clientCommands;
