3c9b8e670928edbf1c5f804214dc4b81c1fd01a8
AkshayMohan <akshayceal@gmail.com>
2020-05-02 14:30:13 +0530

    Restructured commands.js

--- src/server/commands.js
+++ src/server/commands.js
@@ -1,9 +1,20 @@
 const clientCommands = {
 
-	//command:				command function to be invoked.
-	'/join':				cmd_join
+	'/join': {
+
+		execute:		cmd_join,
+		syntax:			'/join [nickname]',
+		description:	'Joins user to the chat using the nickname provided.'
+	},
+	'/help': {
+
+		execute:		cmd_help,
+		syntax:			'/help [cmd]',
+		description:	'Displays description of an existing command.'
+	}
 }
 
+//#TODO : Return codes.
 function cmd_join(client, socket, params) {
 
 	if(typeof client.clientName === 'string') {
@@ -13,18 +24,36 @@ function cmd_join(client, socket, params) {
 	}
 	if(params === null) {
 
-		socket.emit('server-message', 'USAGE : /join [nickname]');
+		socket.emit('server-message', 'USAGE : ' + clientCommands['/join'].syntax);
 		return 1;
+	}
+	client.clientName = params;
+	socket.join('chat-room');
+
+	socket.emit('server-message', 'Your nickname is now ' + params);
+
+	//Broadcast messages are sent to everyone except the client.
+	socket.broadcast.to('chat-room').emit('server-message', params + ' has joined the chat!');
+	return 0;
+}
+
+function cmd_help(client, socket, params) {
+
+	if(params === null) {
+
+		socket.emit('server-message', 'USAGE : ' + clientCommands['/help'].syntax);
+		return 1;
+	}
+	if(!(params in clientCommands)) {
+
+		socket.emit('server-message', 'This command does not exist.');
 	} else {
 
-		client.clientName = params;
-		socket.join('chat-room');
-
-		socket.emit('server-message', 'Your nickname is now ' + params);
-
-		//Broadcast messages are sent to everyone except the client.
-		socket.broadcast.to('chat-room').emit('server-message', params + ' has joined the chat!');
-		return 0;
+		socket.emit('server-message', `
+			USAGE : ${clientCommands[params].syntax}\n\
+			<br/>\n\
+			${clientCommands[params].description}\n`);
 	}
+	return 0;
 }
 module.exports = clientCommands;
